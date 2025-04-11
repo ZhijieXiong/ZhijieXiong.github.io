a697878c9d2b3a5ca9ad6ad8f0abe92bf26a1439
ZhijieXiong <18800118477@163.com>
2025-04-11 08:22:02 +0800

    update

--- sub-page/pyedmine/rankingList.js
+++ sub-page/pyedmine/rankingList.js
@@ -44,6 +44,7 @@ const MODEL_TYPE = {
   KT: {
     "Concept Level": ["DKT", "DKVMN", "DKTForget", "ATKT"],
     "Question Level": ["qDKT", "AKT", "SimpleKT", "SparseKT", "DIMKT", "LPKT", "LBKT", "MIKT", "QIKT", "QDCKT", "CKT", "HDLPKT", "DTransformer", "ABQR", "ATDKT", "HawkesKT"],
+    "No Side Info (Question Level)": ["qDKT", "AKT", "SimpleKT", "SparseKT", "DIMKT", "MIKT", "QIKT", "QDCKT", "CKT", "DTransformer", "ABQR", "ATDKT", "HawkesKT"],
     // 添加更多类型...
   },
   CD: {
